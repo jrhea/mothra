d0066fe7ce337dba0e98e896aac1f954951bed5d
jonny rhea <jonathan.rhea@gmail.com>
2020-04-06 09:58:12 -0500

    moved config() to matches() in networkconfig

--- core/src/api/network.rs
+++ core/src/api/network.rs
@@ -1,6 +1,4 @@
 use super::error;
-use clap::ArgMatches;
-use clap::{App, AppSettings, Arg};
 use futures::prelude::*;
 use futures::Stream;
 use libp2p_wrapper::Service as LibP2PService;
@@ -10,7 +8,6 @@ use libp2p_wrapper::{NetworkConfig, RPCErrorResponse, RPCEvent, RPCRequest, RPCR
 use parking_lot::Mutex;
 use slog::{debug, info, o, warn};
 use std::sync::Arc;
-use std::process;
 use tokio::runtime::TaskExecutor;
 use tokio::sync::{mpsc, oneshot};
 
@@ -31,16 +28,16 @@ pub struct Network {
 
 impl Network {
     pub fn new(
-        args_vec: Vec<String>,
+        args: Vec<String>,
         executor: &TaskExecutor,
         discovered_peer: DiscoveredPeerType,
         receive_gossip: ReceiveGossipType,
         receive_rpc: ReceiveRpcType,
         log: slog::Logger,
     ) -> error::Result<Self> {
-        let args = config(args_vec);
+        let arg_matches = NetworkConfig::matches(args);
         let mut config = NetworkConfig::new();
-        config.apply_cli_args(&args).unwrap();
+        config.apply_cli_args(&arg_matches).unwrap();
         // build the network channel
         let (network_send, network_recv) = mpsc::unbounded_channel::<NetworkMessage>();
         // launch libp2p Network
@@ -248,103 +245,6 @@ fn network_service(
         }
         Ok(Async::NotReady)
     })
-}
-
-fn config(args: Vec<String>) -> ArgMatches<'static> {
-    App::new("Mothra")
-    .version("0.0.1")
-    .author("Your Mom")
-    .about("LibP2P for Dummies")
-    .setting(AppSettings::TrailingVarArg)
-    .setting(AppSettings::DontDelimitTrailingValues)
-    .arg(
-        Arg::with_name("datadir")
-            .long("datadir")
-            .value_name("DIR")
-            .help("Data directory for keys and databases.")
-            .takes_value(true)
-    )
-    // network related arguments
-    .arg(
-        Arg::with_name("listen-address")
-            .long("listen-address")
-            .value_name("ADDRESS")
-            .help("The address the client will listen for UDP and TCP connections. (default 127.0.0.1).")
-            .default_value("127.0.0.1")
-            .takes_value(true),
-    )
-    .arg(
-        Arg::with_name("port")
-            .long("port")
-            .value_name("PORT")
-            .help("The TCP/UDP port to listen on. The UDP port can be modified by the --discovery-port flag.")
-            .takes_value(true),
-    )
-    .arg(
-        Arg::with_name("maxpeers")
-            .long("maxpeers")
-            .help("The maximum number of peers (default 10).")
-            .default_value("10")
-            .takes_value(true),
-    )
-    .arg(
-        Arg::with_name("boot-nodes")
-            .long("boot-nodes")
-            .allow_hyphen_values(true)
-            .value_name("ENR-LIST")
-            .help("One or more comma-delimited base64-encoded ENR's to bootstrap the p2p network.")
-            .takes_value(true),
-    )
-    .arg(
-        Arg::with_name("discovery-port")
-            .long("disc-port")
-            .value_name("PORT")
-            .help("The discovery UDP port.")
-            .default_value("9000")
-            .takes_value(true),
-    )
-    .arg(
-        Arg::with_name("discovery-address")
-            .long("discovery-address")
-            .value_name("ADDRESS")
-            .help("The IP address to broadcast to other peers on how to reach this node.")
-            .takes_value(true),
-    )
-    .arg(
-        Arg::with_name("topics")
-            .long("topics")
-            .value_name("STRING")
-            .help("One or more comma-delimited gossipsub topic strings to subscribe to.")
-            .takes_value(true),
-    )
-        .arg(
-        Arg::with_name("libp2p-addresses")
-            .long("libp2p-addresses")
-            .value_name("MULTIADDR")
-            .help("One or more comma-delimited multiaddrs to manually connect to a libp2p peer without an ENR.")
-            .takes_value(true),
-        )
-    .arg(
-        Arg::with_name("debug-level")
-            .long("debug-level")
-            .value_name("LEVEL")
-            .help("Possible values: info, debug, trace, warn, error, crit")
-            .takes_value(true)
-            .possible_values(&["info", "debug", "trace", "warn", "error", "crit"])
-            .default_value("info"),
-    )
-    .arg(
-        Arg::with_name("verbosity")
-            .short("v")
-            .multiple(true)
-            .help("Sets the verbosity level")
-            .takes_value(true),
-    )
-   .get_matches_from_safe(args.iter())
-        .unwrap_or_else(|e| {
-            eprintln!("{}", e);
-            process::exit(1);
-        })
 }
 
 /// Types of messages that the network Network can receive.
--- core/src/libp2p-wrapper/src/config.rs
+++ core/src/libp2p-wrapper/src/config.rs
@@ -1,10 +1,11 @@
-use clap::ArgMatches;
+use clap::{App, AppSettings, Arg, ArgMatches};
 use enr::Enr;
 use libp2p::gossipsub::{GossipsubConfig, GossipsubConfigBuilder};
 use libp2p::Multiaddr;
 use serde_derive::{Deserialize, Serialize};
 use std::path::PathBuf;
 use std::time::Duration;
+use std::process;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 #[serde(default)]
@@ -75,6 +76,103 @@ impl Default for Config {
 impl Config {
     pub fn new() -> Self {
         Config::default()
+    }
+
+    pub fn matches(args: Vec<String>) -> ArgMatches<'static> {
+        App::new("Mothra")
+        .version("0.0.1")
+        .author("Your Mom")
+        .about("LibP2P for Dummies")
+        .setting(AppSettings::TrailingVarArg)
+        .setting(AppSettings::DontDelimitTrailingValues)
+        .arg(
+            Arg::with_name("datadir")
+                .long("datadir")
+                .value_name("DIR")
+                .help("Data directory for keys and databases.")
+                .takes_value(true)
+        )
+        // network related arguments
+        .arg(
+            Arg::with_name("listen-address")
+                .long("listen-address")
+                .value_name("ADDRESS")
+                .help("The address the client will listen for UDP and TCP connections. (default 127.0.0.1).")
+                .default_value("127.0.0.1")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("port")
+                .long("port")
+                .value_name("PORT")
+                .help("The TCP/UDP port to listen on. The UDP port can be modified by the --discovery-port flag.")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("maxpeers")
+                .long("maxpeers")
+                .help("The maximum number of peers (default 10).")
+                .default_value("10")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("boot-nodes")
+                .long("boot-nodes")
+                .allow_hyphen_values(true)
+                .value_name("ENR-LIST")
+                .help("One or more comma-delimited base64-encoded ENR's to bootstrap the p2p network.")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("discovery-port")
+                .long("disc-port")
+                .value_name("PORT")
+                .help("The discovery UDP port.")
+                .default_value("9000")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("discovery-address")
+                .long("discovery-address")
+                .value_name("ADDRESS")
+                .help("The IP address to broadcast to other peers on how to reach this node.")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("topics")
+                .long("topics")
+                .value_name("STRING")
+                .help("One or more comma-delimited gossipsub topic strings to subscribe to.")
+                .takes_value(true),
+        )
+            .arg(
+            Arg::with_name("libp2p-addresses")
+                .long("libp2p-addresses")
+                .value_name("MULTIADDR")
+                .help("One or more comma-delimited multiaddrs to manually connect to a libp2p peer without an ENR.")
+                .takes_value(true),
+            )
+        .arg(
+            Arg::with_name("debug-level")
+                .long("debug-level")
+                .value_name("LEVEL")
+                .help("Possible values: info, debug, trace, warn, error, crit")
+                .takes_value(true)
+                .possible_values(&["info", "debug", "trace", "warn", "error", "crit"])
+                .default_value("info"),
+        )
+        .arg(
+            Arg::with_name("verbosity")
+                .short("v")
+                .multiple(true)
+                .help("Sets the verbosity level")
+                .takes_value(true),
+        )
+       .get_matches_from_safe(args.iter())
+            .unwrap_or_else(|e| {
+                eprintln!("{}", e);
+                process::exit(1);
+            })
     }
 
     pub fn apply_cli_args(&mut self, args: &ArgMatches) -> Result<(), String> {
